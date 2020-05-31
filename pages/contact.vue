a7c04be2e08e0dde0396af1f1923833934252a8d
nourhen <nourheneebarhoumii@gmail.com>
2020-05-31 17:39:49 +0200

    contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -15,7 +15,7 @@
           </div>
           <div class="col-md-7">
 
-            <form action="#" method="post">
+            <form  method="post" data-netlify="true">
               
               <div class="p-3 p-lg-5 border">
                 <div class="form-group row">
